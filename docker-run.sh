460a775b1a2f7b5257eb1922d1dbd87db747ff70
Ramon Wijnands <rayman747@hotmail.com>
2019-09-01 15:42:46 +0200

    Set the Youtube API key from an environment variable

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,7 @@ docker pull "$IMAGE"
 docker run --network host \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://hasurauser:hasurauser@localhost/hasura \
        -e FUSIC_BACKEND_URL \
+       -e FUSIC_YOUTUBE_KEY \
        --name hasura \
        -d \
        "$IMAGE"
