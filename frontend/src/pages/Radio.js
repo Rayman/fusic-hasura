1f0f3c058e00ea979827c15708ea513ce1f7ac8a
Ramon Wijnands <rayman747@hotmail.com>
2019-09-07 01:51:58 +0200

    Scaffold an radio edit dialog

--- frontend/src/pages/Radio.js
+++ frontend/src/pages/Radio.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { useQuery, useSubscription } from '@apollo/react-hooks';
+import PropTypes from 'prop-types';
+
+import { Edit } from 'react-feather';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import './Radio.css';
 
@@ -55,11 +61,46 @@ function AddSong() {
   );
 }
 
+function EditRadio({ show, onHide, radio: { id, title, artwork_url } }) {
+  function onSave(e) {}
+
+  return (
+    <>
+      <Modal show={show} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={onSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+EditRadio.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+  radio: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    artwork_url: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 export default function Radio({
   match: {
     params: { id },
   },
 }) {
+  const [edit, setEdit] = useState(false);
+
   const { loading, data, error } = useSubscription(RADIOS_SUBSCRIPTION, {
     variables: { id },
   });
@@ -67,20 +108,26 @@ export default function Radio({
   if (loading) return 'Loading...';
   if (error) throw error;
 
-  const {
-    radio_by_pk: { title, created_at, artwork_url },
-  } = data;
+  const { radio_by_pk: radio } = data;
+  const { title, created_at, artwork_url } = radio;
 
   return (
     <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1>{title}</h1>
-        <p class="lead">Created {created_at}</p>
+        <h1>
+          {title}{' '}
+          <Button variant="link" onClick={() => setEdit(true)}>
+            <Edit />
+          </Button>
+        </h1>
+
+        <p className="lead">Created {created_at}</p>
       </div>
       <AddSong />
       <ul>
         <li>song</li>
       </ul>
+      {<EditRadio show={edit} onHide={() => setEdit(false)} radio={radio} />}
     </main>
   );
 }
