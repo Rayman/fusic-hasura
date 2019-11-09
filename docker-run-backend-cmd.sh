2fea43db4c8125b947e869541c9811988cb97022
Ramon Wijnands <rayman747@hotmail.com>
2019-11-09 01:36:25 +0100

    Update docker scripts for authorization

--- docker-run-backend-cmd.sh
+++ docker-run-backend-cmd.sh
@@ -5,10 +5,10 @@ echo "hosting graphql on port $PORT"
 
 BACKEND_PORT="$((PORT+1))"
 
+PORT="$BACKEND_PORT" npm --prefix backend start & # un in the background
+sleep 3
 FUSIC_BACKEND_URL="http://localhost:$BACKEND_PORT" graphql-engine \
   --database-url ${DATABASE_URL?"env DATABASE_URL missing"} \
   serve \
-  --server-port "$PORT" \
-  & # run in the background
+  --server-port "$PORT"
 
-PORT="$BACKEND_PORT" npm --prefix backend start
